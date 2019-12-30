ec302b57da93b0307940d6c8212aa11d652dd958
admin <365363340@qq.com>
2019-12-30 23:33:04 +0800

    day04

---
+++ xc-ui-pc-sysmanage/src/module/cms/page/page_list.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <!--查询表单-->
+    <el-form :model="params">
+      <el-select v-model="params.siteId" placeholder="请选择站点">
+        <el-option
+          v-for="item in siteList"
+          :key="item.siteId"
+          :label="item.siteName"
+          :value="item.siteId">
+        </el-option>
+      </el-select>
+      页面别名：<el-input v-model="params.pageAliase"  style="width: 100px"></el-input>
+      <el-button type="primary" v-on:click="query"  size="small">查询</el-button>
+      <router-link class="mui-tab-item" :to="{path:'/cms/page/add/',query:{
+          page: this.params.page,
+          siteId: this.params.siteId}}">
+        <el-button  type="primary" size="small">新增页面</el-button>
+      </router-link>
+    </el-form>
+
+    <el-table
+      :data="list"
+      stripe
+      style="width: 100%">
+      <el-table-column type="index" width="40">
+      </el-table-column>
+      <el-table-column prop="pageName" label="页面名称" width="120">
+      </el-table-column>
+      <el-table-column prop="pageAliase" label="别名" width="120">
+      </el-table-column>
+      <el-table-column prop="pageType" label="页面类型" width="100">
+      </el-table-column>
+      <el-table-column prop="pageWebPath" label="访问路径" width="250">
+      </el-table-column>
+      <el-table-column prop="pagePhysicalPath" label="物理路径" width="250">
+      </el-table-column>
+      <el-table-column prop="pageCreateTime" label="创建时间" width="180" >
+      </el-table-column>
+      <el-table-column label="操作" width="150">
+        <template slot-scope="page">
+          <el-button
+            size="small"type="text"
+            @click="edit(page.row.pageId)">编辑
+          </el-button>
+          <el-button
+            size="small"type="text"
+            @click="del(page.row.pageId)">删除
+          </el-button>
+          <el-button @click="preview(page.row.pageId)" type="text" size="small">页面预览</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      layout="prev, pager, next"
+      :total="total"
+      :page-size="params.size"
+      :current-page="params.page"
+      v-on:current-change="changePage"
+      style="float:right">
+    </el-pagination>
+  </div>
+</template>
+<script>
+  /*编写页面静态部分，即model及vm部分。*/
+  import * as cmsApi from '../api/cms'
+  export default {
+    data() {
+      return {
+        siteList:[],//站点列表
+        list: [],
+        total:0,
+        params:{
+          page:1,
+          size:10
+        }
+      }
+    },
+    methods:{
+      query:function(){
+        // alert('查询')
+        //调用服务端的接口
+        cmsApi.page_list(this.params.page,this.params.size,this.params).then((res)=>{
+          //将res结果数据赋值给数据模型对象
+          this.list = res.queryResult.list;
+          this.total = res.queryResult.total;
+        })
+      },
+      changePage:function(page){//形参就是当前页码
+        //调用query方法
+        // alert(page)
+        this.params.page = page;
+        this.query()
+      },
+
+      //查询Site
+      querySite:function () {
+        cmsApi.querySite().then((res) => {
+          this.siteList = res.queryResult.list;
+        })
+      },
+
+      //修改
+      edit:function (pageId) {
+        this.$router.push({ path: '/cms/page/edit/'+pageId,query:{
+            page: this.params.page,
+            siteId: this.params.siteId}})
+      },
+
+      //删除
+      del:function(pageId){
+        //确认提示
+        this.$confirm('确认提交吗？', '提示', {}).then(() => {
+          //调用page方法请求服务端的新增页面接口
+          cmsApi.page_del(pageId).then((res) =>{
+            //解析响应内容
+            if(res.success){
+              this.$message({
+                message: '提交成功',
+                type: 'success'
+              });
+              this.query();
+            }else{
+              this.$message.error('提交失败');
+            }
+          })
+        })
+      },
+
+      //页面预览
+      preview(pageId){
+        window.open("http://www.xuecheng.com/cms/preview/"+pageId)
+      }
+    },
+    created(){
+      //从路由上获取参数
+      this.params.page = Number.parseInt(this.$route.query.page||1);
+      this.params.siteId = this.$route.query.siteId||'';
+    },
+    mounted(){
+      //当DOM元素渲染完成后调用query
+      this.query();
+      //初始化站点列表
+      this.querySite();
+    }
+  }
+</script>
+<style>
+  /*编写页面样式，不是必须*/
+</style>
